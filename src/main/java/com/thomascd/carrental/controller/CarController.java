d3a8b8e3568101fc4f04410bb1854779464b1f57
ThomasCattarossiD <pro.thomascd@gmail.com>
2025-04-17 10:02:09 +0200

    Add addCar method to CarController for car registration handling

--- src/main/java/com/thomascd/carrental/controller/CarController.java
+++ src/main/java/com/thomascd/carrental/controller/CarController.java
@@ -1,9 +1,11 @@
 package com.thomascd.carrental.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,4 +35,13 @@ public class CarController {
     public void returnCar(@PathVariable String registrationNumber) {
         carRentalService.returnCar(registrationNumber);
     }
+
+    @PostMapping("/add")
+    public ResponseEntity<String> addCar(@RequestBody Car car) {
+    if (carRentalService.addCar(car)) {
+        return ResponseEntity.ok("Voiture ajoutée avec succès");
+    }
+    return ResponseEntity.badRequest().body("Le numéro d'immatriculation existe déjà");
+}
+
 }
